9451ac89e74e1f6d1d10fa824a9bac3d11988e0a
Afzal Jasani <amj355@nyu.edu>
2025-05-23 17:19:14 -0700

    update dimbroker

--- models/silver/DimBroker.sql
+++ models/silver/DimBroker.sql
@@ -16,9 +16,11 @@ SELECT
   employeeoffice office,
   employeephone phone,
   true iscurrent,
-  0 batchid,
+  12 batchid,
   (SELECT min(to_date(datevalue)) as effectivedate FROM tcloud_tpcdi.DimDate) effectivedate,
   date('9999-12-31') enddate,
   concat(brokerid, '-', enddate) as sk_brokerid
 FROM  tpcdi.tpcdi_100_dbsql_100_stage.v_hr
 WHERE employeejobcode = 314
+
+
